896c3027daeca7d0f0a7ac6d9fd6f08e1478fa39
LucasKienast <lucas.kienast@hotmail.de>
2023-11-22 10:04:49 +0000

    object size calculator

---
+++ src/fields/ObjectSizeCalculator.java
@@ -0,0 +1,66 @@
+package fields;
+
+import java.lang.reflect.*;
+
+public class ObjectSizeCalculator {
+    private static final long HEADER_SIZE = 12;
+    private static final long REFERENCE_SIZE = 4;
+
+    public long sizeOfObject(Object input) {
+        long size = HEADER_SIZE + REFERENCE_SIZE;
+        Class<?> clazz = input.getClass();
+
+        for (Field field : clazz.getDeclaredFields()) {
+            if (!field.isAccessible()) {
+                field.setAccessible(true);
+            }
+
+            if (field.getType().isPrimitive()) {
+                size += sizeOfPrimitiveType(field.getType());
+            }
+            else if (field.getType().equals(String.class)) {
+                try {
+                    String fieldValue = (String) field.get(input);
+
+                    if (fieldValue != null) {
+                        size += sizeOfString(fieldValue);
+                    } else {
+                        size += REFERENCE_SIZE;
+                    }
+
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Error accessing field value", e);
+                }
+            }
+            else {
+                size += REFERENCE_SIZE;
+            }
+        }
+
+        return  size;
+    }
+
+
+    /*************** Helper methods ********************************/
+    private long sizeOfPrimitiveType(Class<?> primitiveType) {
+        if (primitiveType.equals(int.class)) {
+            return 4;
+        } else if (primitiveType.equals(long.class)) {
+            return 8;
+        } else if (primitiveType.equals(float.class)) {
+            return 4;
+        } else if (primitiveType.equals(double.class)) {
+            return 8;
+        } else if (primitiveType.equals(byte.class)) {
+            return 1;
+        } else if (primitiveType.equals(short.class)) {
+            return 2;
+        }
+        throw new IllegalArgumentException(String.format("Type: %s is not supported", primitiveType));
+    }
+
+    private long sizeOfString(String inputString) {
+        int stringBytesSize = inputString.getBytes().length;
+        return HEADER_SIZE + REFERENCE_SIZE + stringBytesSize;
+    }
+}
