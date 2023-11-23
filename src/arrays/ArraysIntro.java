a5f2491ba6f6186e0abbe500a3857ebe596c3b44
LucasKienast <lucas.kienast@hotmail.de>
2023-11-23 14:02:55 +0000

    arrays reflection

---
+++ src/arrays/ArraysIntro.java
@@ -0,0 +1,42 @@
+package arrays;
+
+import java.lang.reflect.Array;
+
+public class ArraysIntro {
+
+    public static void main(String[] args) {
+        int[] oneDimensionalArray = {1, 2};
+        inspectArrayValues(oneDimensionalArray);
+
+        double[][] twoDimensionalArray = {{1.5, 2.5}, {3.5, 4.5}};
+        inspectArrayValues(twoDimensionalArray);
+    }
+
+    public static void inspectArrayValues(Object arrayObject) {
+        int arrayLength = Array.getLength(arrayObject);
+        System.out.print("[");
+
+        for (int i = 0; i < arrayLength; i++) {
+            Object element = Array.get(arrayObject, i);
+
+            if (element.getClass().isArray())
+                inspectArrayValues(element);
+            else
+                System.out.print(element);
+
+            if (i != arrayLength - 1)
+                System.out.print(" , ");
+        }
+
+        System.out.print("]");
+    }
+
+    public static void inspectArrayObject(Object arrayObject) {
+        Class<?> clazz = arrayObject.getClass();
+        System.out.println(String.format("Is array: %s", clazz.isArray()));
+
+        Class<?> arrayComponentType = clazz.getComponentType();
+        System.out.println(String.format("This is an array of type: %s", arrayComponentType.getTypeName()));
+    }
+
+}
